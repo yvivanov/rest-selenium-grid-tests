c196c5d583e1edb53b9f8876d5963020a0e865ef
Yuriy V. Ivanov <yvivanov@yahoo.com>
2017-04-07 11:33:06 -0700

    fix

--- src/main/java/com/ajourdesign/application/Mini.java
+++ src/main/java/com/ajourdesign/application/Mini.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import org.yaml.snakeyaml.Yaml;
 
-public class MiniGet extends Thread {
+public class Mini extends Thread {
     private Socket          S;
     private static String   HOST, ROOT;
 
@@ -19,17 +19,17 @@ public class MiniGet extends Thread {
                         ROOT = (String) conf.get("ROOT");
         ServerSocket    sckt = new ServerSocket ( PORT );
         System.out.println("HOST: "+ HOST +"\nPORT: "+ PORT +"\nROOT: "+ ROOT );
-        while( true ) new MiniGet( sckt.accept() ).start();
+        while( true ) new Mini( sckt.accept() ).start();
     }
 
-    public MiniGet( Socket s ) throws Exception {
+    public Mini( Socket s ) throws Exception {
         S = s;
     }
 
     public void run() {
         String  id = this.toString();
         try {
-            BufferedReader  I = new BufferedReader( new InputStreamReader( S.getInputStream()));
+            BufferedReader  I = new BufferedReader(new InputStreamReader(S.getInputStream()));
             String          X = I.readLine();
             if( X == null ) {
                 I.close();
@@ -37,19 +37,19 @@ public class MiniGet extends Thread {
                 Log( "*** "+ id );
                 return;
             }
-            PrintWriter O = new PrintWriter( S.getOutputStream(), true );	// autoFlush
+            PrintWriter O = new PrintWriter(S.getOutputStream(), true); // autoFlush
             int         ContentLength = 0;
             String      UserAgent     = null,
                         Host          = null,
-                        L, s;
+                        L, u;
             Log( ">>> "+ id +"\r\n\t"+ X );
             while( (L = I.readLine()) != null  &&  L.length() > 0 ) {
-                s = L.toUpperCase();
+                u = L.toUpperCase();
                 Log( "\t"+ L );
                 //                0....+....1....+.     <header>
-                if( s.startsWith("CONTENT-LENGTH: "))   ContentLength = Integer.parseInt( L.substring( 16 ) ); else
-                if( s.startsWith("USER-AGENT: "    ))   UserAgent     = L.substring( 12 );                     else
-                if( s.startsWith("HOST: "          ))   Host          = L.substring(  6 );
+                if( u.startsWith("CONTENT-LENGTH: "))   ContentLength = Integer.parseInt( L.substring( 16 ) ); else
+                if( u.startsWith("USER-AGENT: "    ))   UserAgent     = L.substring( 12 );                     else
+                if( u.startsWith("HOST: "          ))   Host          = L.substring(  6 );
             }
             Log( Time_Stamp()                                 +"\t"+
                     S.getRemoteSocketAddress()                +"\t"+
@@ -59,7 +59,7 @@ public class MiniGet extends Thread {
                     ContentLength                             +"]\t"+
                     UserAgent );
 
-            switch( "GET POSTHEADOPTI".indexOf( X.substring( 0, 4 ).toUpperCase())) {
+            switch("GET POSTHEADOPTI".indexOf( X.substring( 0, 4 ).toUpperCase())) {
             //       0....+....1....+....2....+....3..
                 case 0:  Get ( id, X, O, S.getOutputStream(), false );	break;
             //  case 4:  Post( id, I, O, ContentLength );             	break;
@@ -82,18 +82,18 @@ public class MiniGet extends Thread {
     }
 
     private void Get( String id, String x, PrintWriter o, OutputStream O, boolean HEAD ) {
-        String[] y = x.split("\\s");
-        x = y[1];
+        String[] frag = x.split("\\s");
+        String   path = frag[1];
         try {
             int                 n;
             byte                b[] = new byte[8192];
-            File                src = new File( ROOT + x );
+            File                src = new File( ROOT + path );
             boolean             htm = false;
             DataInputStream     dis = new DataInputStream(new BufferedInputStream(new FileInputStream( src ), 8192));
             DataOutputStream    dos = new DataOutputStream( O );
 
             if( src.canRead() ) {
-                switch("HTML.HTM.JS.CSS.ICO.GIF.JPG.PNG.XLS.CSV.TSV".indexOf( x.toUpperCase().substring( x.lastIndexOf(".")+1 ))) {
+                switch("HTML.HTM.JS.CSS.ICO.GIF.JPG.PNG.XLS.CSV.TSV".indexOf(path.toUpperCase().substring(path.lastIndexOf(".")+1))) {
                 //      0....+....1....+....2....+....3....+....4
                     case 0:
                     case 5:  Response( id, "200 OK", "text/html",           	 src.length(), o, null );htm=true;break;
@@ -113,13 +113,17 @@ public class MiniGet extends Thread {
                     while( (n = dis.read( b )) > 0 )
                        dos.write( (htm ? (new String( b )).replaceFirst( "HOST", HOST ).getBytes() : b), 0, n );
             }
-            else Response( id, "403 Forbidden", "text/html", 0, o, Wrn("<blink>Forbidden: </blink>"+ x ));
+            else Response( id, "403 Forbidden", "text/html", 0, o, Wrn("<blink>Forbidden: </blink>"+ path ));
             dos.flush();
             Thread.sleep(256);
             dos.close();
             dis.close();
         }
         catch( IOException e ) {
+            if( path.equals("/HALT") ) {
+                Response( id, "200 OK", "text/html", 0, o, Wrn("<blink>Mini is HALTED</blink>"));
+                System.exit(0);
+            }
             Log("I/O 3: "+ e );
             Response( id, "404 Not Found", "text/html", 0, o, Wrn("<blink>Not Found</blink>"));
         }
@@ -131,19 +135,17 @@ public class MiniGet extends Thread {
     private void Response( String id, String code, String type, long length, PrintWriter o, String text ) {
         Log("... "+ id +"\r\n"+
             "\tHTTP/1.1 "+ code +"\r\n\tDate: "+ (new java.util.Date()) +
-            "\r\n\tServer: J3W 2017-04-10\r\n\tContent-Length: "+ (text == null ? length : text.length()) +
+            "\r\n\tServer: Mini 2017-04-10\r\n\tContent-Length: "+ (text == null ? length : text.length()) +
             "\r\n\tContent-Type: "+ type );
         o.print(
             "HTTP/1.1 "+ code +"\r\nDate: "+ (new java.util.Date()) +
-            "\r\nServer: J3W 2017-04-10\r\nContent-Length: "+ (text == null ? length : text.length()) +
+            "\r\nServer: Mini 2017-04-10\r\nContent-Length: "+ (text == null ? length : text.length()) +
             "\r\nContent-Type: "+ type +"\r\n\r\n" );
         if( text != null ) o.print( text );
         o.flush();
     }
 
-    private String Wrn( String s ) {
-        return "<html><title>J3W</title><body><h1><br>"+ s +"</h1></body></html>";
-    }
+    private String Wrn( String s ) {return "<html><title>Mini</title><body><h1><br>"+ s +"</h1></body></html>";}
 
     private static void Log( String s ) {
         System.err.println( s );
